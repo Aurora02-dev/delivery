5beb24a7b2957e98f3d2b20254e57fad6ea4991f
[Ajungo Caleb Christian] <[calebchristian.a24@gmail.com]>
2026-06-26 13:55:45 +0300

    Made the login screen interacive

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,7 @@
-﻿import 'package:flutter/material.dart';
+﻿import 'package:delivery/register.dart';
+import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,10 +15,29 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController passwordController = TextEditingController();
   bool _obscurePassword = true;
 
+  Future<void> openWebsite(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+      )) {
+    } else {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login'), centerTitle: true),
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          color: Color.fromARGB(255, 255, 81, 0),
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
@@ -24,20 +45,27 @@ class _LoginScreenState extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
-                'Welcome to Our AI-Powered Legal Assistant',
+                'Welcome to Our AI-Powered Legal \nAssistant',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Color.fromARGB(255, 255, 81, 0),
+                  color: Color.fromARGB(255, 37, 37, 37),
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(height: 32),
+              
+              const SizedBox(height: 40),
+
               TextField(
                 controller: emailController,
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 236, 79, 12),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
                   hintText: 'Enter your email',
                   prefixIcon: const Icon(Icons.email),
                   border: OutlineInputBorder(
@@ -51,6 +79,11 @@ class _LoginScreenState extends State<LoginScreen> {
                 obscureText: _obscurePassword,
                 decoration: InputDecoration(
                   labelText: 'Password',
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 236, 79, 12),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
                   hintText: 'Enter your password',
                   prefixIcon: const Icon(Icons.lock),
                   suffixIcon: IconButton(
@@ -70,7 +103,28 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              const SizedBox(height: 24),
+
+              const SizedBox(height: 5),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    // Navigate to Forgot Password page
+                  },
+                  child: const Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 236, 79, 12),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
@@ -83,89 +137,83 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   child: const Text(
                     'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
+                    style: TextStyle(color: Colors.white, fontSize: 26),
                   ),
                 ),
               ),
 
               Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-    const Text(
-      "Don't Have an Account? ",
-    ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't Have An Account? "),
 
-    GestureDetector(
-      onTap: () {
-        // Navigate to Sign Up page
-      },
-      child: const Text(
-        "Sign Up",
-        style: TextStyle(
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-  ],
-),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigate to Register Screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ),
+                        );
+                    },
+                    child: MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
 
-const SizedBox(height: 25),
+              const SizedBox(height: 25),
 
-Row(
-  children: [
-    Expanded(
-      child: Divider(
-        thickness: 1,
-      ),
-    ),
+              Row(
+                children: [
+                  Expanded(child: Divider(thickness: 1)),
 
-    Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Text(
-        "OR",
-        style: TextStyle(
-          color: Colors.grey,
-        ),
-      ),
-    ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("OR", style: TextStyle(color: Colors.grey)),
+                  ),
 
-    Expanded(
-      child: Divider(
-        thickness: 1,
-      ),
-    ),
-  ],
-),
+                  Expanded(child: Divider(thickness: 1)),
+                ],
+              ),
 
-Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-    IconButton(
-      onPressed: () {},
-      icon: const FaIcon(
-        FontAwesomeIcons.google,
-      ),
-    ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      openWebsite('https://www.google.com/');
+                    },
+                    icon: const FaIcon(FontAwesomeIcons.google),
+                  ),
 
-    IconButton(
-      onPressed: () {},
-      icon: const FaIcon(
-        FontAwesomeIcons.facebook,
-      ),
-    ),
+                  IconButton(
+                    onPressed: () {
+                      openWebsite('https://www.facebook.com/');
+                      },
+                    icon: const FaIcon(FontAwesomeIcons.facebook),
+                  ),
 
-    IconButton(
-      onPressed: () {},
-      icon: const FaIcon(
-        FontAwesomeIcons.xTwitter,
-      ),
-    ),
-  ],
-),
+                  IconButton(
 
-const SizedBox(height: 20),
+                    onPressed: () {
+                      openWebsite('https://twitter.com/');
+                    },
+                    icon: const FaIcon(FontAwesomeIcons.xTwitter),
+                  ),
+                ],
+              ),
 
+              const SizedBox(height: 20),
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:delivery/register.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,15 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
       home: Scaffold(
         body: ListView(
           children: [
             Image.network(
               "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDJubwfrdBsUB1CIV_mwGQk2ABY3pd08cUNfHmHvaJ_Q&s=10",
             ),
+
             SizedBox(
               height: 100,
             ), // Add some spacing between the image and text
+
             Text(
               "AI-Powered Legal Assistant",
               textAlign: TextAlign.center,
